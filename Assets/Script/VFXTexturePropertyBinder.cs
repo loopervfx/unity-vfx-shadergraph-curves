4ce0ae9e961ca882beabd96534c6b09611c2a805
landonth <landonth@users.noreply.github.com>
2020-01-27 00:18:56 -0800

    added scene, vfx, rendertexture, propertybinder

---
+++ Assets/Script/VFXTexturePropertyBinder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+
+// The VFXBinder Attribute will populate this class into the property binding's add menu.
+[VFXBinder("Texture/Size")]
+// The class need to extend VFXBinderBase
+public class TextureSizeBinder : VFXBinderBase
+{
+    // VFXPropertyBinding attributes enables the use of a specific
+    // property drawer that populates the VisualEffect properties of a
+    // certain type.
+    [VFXPropertyBinding("System.Single")]
+    public ExposedProperty textureSizeProperty;
+    public Texture target;
+
+    // The IsValid method need to perform the checks and return if the binding
+    // can be achieved.
+    public override bool IsValid(VisualEffect component)
+    {
+        return target != null && component.HasVector2(textureSizeProperty);
+    }
+
+    // The UpdateBinding method is the place where you perform the binding,
+    // by assuming that it is valid. This method will be called only if
+    // IsValid returned true.
+    public override void UpdateBinding(VisualEffect component)
+    {
+        component.SetVector2(textureSizeProperty, new Vector2(target.width, target.height));
+    }
+}
